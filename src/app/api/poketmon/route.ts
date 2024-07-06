1ba847d81922e95d4ca63249841f3c1fadadb699
jonghoon <jonghoon7431@gmail.com>
2024-07-06 17:05:16 +0900

    타입 추가 지정, 주석 제거

--- src/app/api/poketmon/route.ts
+++ src/app/api/poketmon/route.ts
@@ -9,13 +9,11 @@ export const GET = async (request: Request) => {
   try {
     const url: URL = new URL(request.url);
     const page: number = parseInt(url.searchParams.get("page") || "1");
-    // offset: 데이터 페칭 시작점(건너뛸 항목 수)
     const offset: number = (page - 1) * PAGE_PER_ITEM;
-    //limit: 한 페이지당 데이터 수
     const limit: number = Math.min(PAGE_PER_ITEM, TOTAL_POKEMON - offset);
 
     const allPokemonPromises = [...Array(limit)].map((_, index) => {
-      const id = offset + index + 1;
+      const id: number = offset + index + 1;
       return Promise.all([
         axios.get<Pokemon>(`https://pokeapi.co/api/v2/pokemon/${id}`),
         axios.get<Species>(`https://pokeapi.co/api/v2/pokemon-species/${id}`),
